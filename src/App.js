dbf615b2fd56a56d6756debe05857e3b39762d3f
Darren Brooke <darrenbrookebusiness@gmail.com>
2022-05-25 04:07:28 +0100

    add item and search item  commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import Header from './Header';
+import SearchItem from './SearchItem';
+import AddItem from './AddItem';
 import Content from './Content';
 import Footer from './Footer';
 import { useState } from "react";
@@ -6,48 +8,71 @@ import { useState } from "react";
 
 function App() {
 
-  const [items, setItems] = useState([
-    {
-        id: 1,
-        checked: true,
-        item: "Item 1"
-    },
-    {
-        id: 2,
-        checked: false,
-        item: "Item 2"
-    },
-    {
-        id: 3,
-        checked: false,
-        item: "Item 3"
-    }
-  ]);
+  const [items, setItems] = useState(JSON.parse(localStorage.getItem('shoppinglist')));
+
+  const [newItem, setNewItem] = useState('')
+  const [search, setSearch] = useState('')
+
+  const setAndSaveItems = (newItems) => {
+    setItems(newItems);
+    localStorage.setItem('shoppinglist', JSON.stringify(newItems));
+  }
+
+
+  const addItem = (item) => {
+    //uses Ternary statement, it's a simple conditional operator. The ? is if, and : is else
+    //compares the item id against the length - 1 to get the last item in the list as arrays start at 1 then increments it by 1, if id has no length it's 0 and we can just set the id to 1 
+    const id = items.length ? items[items.length - 1].id + 1 : 1;
+    const myNewItem = { id, checked: false, item };
+    // uses spread operator
+    const listItems = [...items, myNewItem];
+    setAndSaveItems(listItems);
+  }
 
   const handleCheck = (id) => {
     // check if item id is = to id, return this new item but it's flipped, if not return existing item.
     const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked } : item);
-    setItems(listItems);
-    localStorage.setItem('shoppinglist', JSON.stringify(listItems));
+    setAndSaveItems(listItems);
+
   }
 
   const handleDelete = (id) => {
     const listItems = items.filter((item) => item.id !== id)
-    setItems(listItems);
-    localStorage.setItem('shoppinglist', JSON.stringify(listItems));
+    setAndSaveItems(listItems);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!newItem) return;
+    // addItem
+    addItem(newItem);
+    setNewItem('');
+
   }
 
   return (
     <div className="App">
       <Header />
-      <Content 
-        items={items}
+
+      <AddItem
+        newItem={newItem}
+        setNewItem={setNewItem}
+        handleSubmit={handleSubmit}
+      />
+      <SearchItem
+        search={search}
+        setSearch={setSearch}
+      />
+      <Content
+        items={items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()))}
         handleCheck={handleCheck}
         handleDelete={handleDelete}
-        />
+      />
       <Footer length={items.length} />
     </div>
   );
+
+
 }
 
 export default App;
